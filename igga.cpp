324bcb239da2801ad3d1f5226a310d2462842de5
Wei-Ting Tu <bluesoul@ms17.hinet.net>
2018-03-02 20:40:33 +0800

    Add comment

--- igga.cpp
+++ igga.cpp
@@ -17,8 +17,8 @@
 class Init {
 public:
     void init(const Jobs& jobs, const Factory& factory);
-    void get_best(Jobs &best, unsigned &best_cost) const;
-    void select(Jobs& pi_incumbent, std::vector<Jobs>& others);
+    void get_best(Jobs &best, unsigned &best_cost) const;       // 取得佳解及其目標函數
+    void select(Jobs& pi_incumbent, std::vector<Jobs>& others); // 進入解構前所選擇之排序
 
 private:
     std::vector<Jobs>     _job_sets;
@@ -26,11 +26,13 @@ private:
     std::vector<double>   _cp;
 };
 
+// 定義data
 void Init::init(const Jobs &jobs, const Factory &factory)
 {
     _job_sets.clear();
     _costs.clear();
 
+    // 分別執行四種演算法以得到四個排序
     PH1 ph1(jobs, factory);
     ph1.run();
 
@@ -43,15 +45,18 @@ void Init::init(const Jobs &jobs, const Factory &factory)
     CFI cfi(jobs, factory);
     cfi.run();
 
+    // 將所得排序存起來
     _job_sets.push_back(ph1.get_result());
     _job_sets.push_back(fnm.get_result());
     _job_sets.push_back(ls.get_result());
     _job_sets.push_back(cfi.get_result());
+    // 將排序之目標函數計算出來並存起來
     _costs.push_back(ph1.get_cost());
     _costs.push_back(fnm.get_cost());
     _costs.push_back(ls.get_cost());
     _costs.push_back(cfi.get_cost());
 
+    // 計算fitness值
     std::vector<double> fits(_costs.size(), 0.0);
 
     for(size_t i = 0; i < _costs.size();++i)
@@ -66,6 +71,7 @@ void Init::init(const Jobs &jobs, const Factory &factory)
     _cp.at(0) = fits.at(0) / total_fits;
     for(size_t i = 1;i < fits.size(); ++i)
     {
+         // 機率計算
         _cp.at(i) = _cp.at(i - 1) + fits.at(i) / total_fits;
     }
 
@@ -97,8 +103,8 @@ void Init::get_best(Jobs& best, unsigned& best_cost) const
         }
     }
 
-    best      = _job_sets.at(min_idx);
-    best_cost = _costs.at(min_idx);
+    best      = _job_sets.at(min_idx); // 將目標函數值最小之排序設為暫存較佳解
+    best_cost = _costs.at(min_idx);    // 目標函數更新
 }
 
 void Init::select(Jobs& pi_incumbent, std::vector<Jobs>& others)
@@ -109,6 +115,7 @@ void Init::select(Jobs& pi_incumbent, std::vector<Jobs>& others)
         return;
     }
 
+    // 隨機產生一個機率(0-1)
     double rf = (double) rand() / (RAND_MAX + 1.0 );
     size_t selected_idx = 0;
 
@@ -120,6 +127,7 @@ void Init::select(Jobs& pi_incumbent, std::vector<Jobs>& others)
         }
     }
 
+    // 選出一個排序pi_incumbent進行解構建構
     pi_incumbent = _job_sets.at(selected_idx);
 
     io_debug("Rf %.2f, select %s(%u) %u\n",
@@ -142,11 +150,12 @@ void Init::select(Jobs& pi_incumbent, std::vector<Jobs>& others)
 
 IGGA::IGGA(const Jobs &jobs, const Factory &factory, unsigned d, double jp)
     : Scheduler(jobs, factory),
-      _d(d),
-      _jp(jp)
+      _d(d),  // 解構所抽出之工件數
+      _jp(jp) // 跳躍機率設定
 {
 }
 
+// 若所得排序pi_purown比建構完之排序pi_new還要優異則更新並進行以下雙點交配
 Jobs IGGA::_crossvoer(const Jobs& pi_new, const Jobs& jobs)
 {
     std::vector<int> indice = Possibility::KnuthShuffle(2, 0, jobs.size() - 1);
@@ -204,6 +213,7 @@ void IGGA::run()
     unsigned pi_best_cost;
     init.get_best(pi_best, pi_best_cost);
 
+    // 終止條件設定
     const unsigned max_count = _jobs.size() * 200;
     const unsigned max_non_improve_count = 100;
 
@@ -275,6 +285,7 @@ void IGGA::run()
 
         io_debug("pi' %u -> %u\n", pi_new_cost, pi_purown_cost);
 
+        // 局部搜尋完所得排序pi_purown與建構完之排序pi_new做比較
         if(pi_purown_cost < pi_new_cost)
         {
             std::vector<Jobs> r;
@@ -353,6 +364,7 @@ void IGGA::run()
     _factory.add_jobs(pi_best);
 }
 
+// 降溫機制
 bool IGGA::_is_accept(unsigned pi_purown, unsigned pi_new) const
 {
     double ap    = exp( -( ( pi_purown - pi_new ) / pi_new ) * 100 );
